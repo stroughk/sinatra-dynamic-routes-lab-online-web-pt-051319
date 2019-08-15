7cfea6771b07641539a4e20c09df37c983522d92
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-15 00:18:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,13 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-get '/reversename/:name' do
-params[:name].reverse
-end
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
 
-get '/square/:number' do
-num = params[:number]
-(num.to_i**2).to_s
-end
+  get '/square/:number' do
+    num = params[:number]
+    (num.to_i**2).to_s
+  end
+
+  get '/say/:number/:phrase' do
+    x = params[:number]
+    x.times.number
+  end
+
 
 end
